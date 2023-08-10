53642224a02ec92dc228ccc611a00d777e335e0a
Hanminjoo72 <hmjhaha0207@naver.com>
2023-08-10 16:39:09 +0900

    practice

--- Chapter/src/ch04/Student.java
+++ Chapter/src/ch04/Student.java
@@ -2,4 +2,14 @@ package ch04;
 
 public class Student {
 
+	int studentId;
+	String studentName;
+	String studentAddress;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," +studentAddress);
+	}
+	public String getStudentName() {
+		return studentName;
+	}
 }
--- Chapter/src/ch04/StudentTest.java
+++ Chapter/src/ch04/StudentTest.java
@@ -4,7 +4,16 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Student studentLee = new Student();
+		studentLee.studentName = "Lee";
+		studentLee.studentAddress = "Seoul";
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "kim";
+		studentKim.studentAddress = "New York";
+		
+		studentLee.showStudentInfo();
+		studentKim.showStudentInfo();
 	}
 
 }
